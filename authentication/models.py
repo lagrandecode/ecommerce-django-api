ffd25873469378d3953e9f6c1a008cd52598050d
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-29 15:04:29 +0100

    "commit"

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    mobile = models.CharField(max_length=14)
+    email = models.EmailField()
+    profile_pics = models.ImageField(upload_to='images/',null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+
+ 

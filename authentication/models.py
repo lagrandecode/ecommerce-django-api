f30af4be017c4c256c7030d73ed50443bec24c8b
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-30 11:40:56 +0100

    "commit"

--- authentication/models.py
+++ authentication/models.py
@@ -8,6 +8,8 @@ class Customer(models.Model):
     mobile = models.CharField(max_length=14)
     email = models.EmailField()
     profile_pics = models.ImageField(upload_to='images/',null=True,blank=True)
+    created_at = models.DateTimeField(auto_created=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication.apps.AuthenticationConfig',
+    'product.apps.ProductConfig',
 ]
 
 MIDDLEWARE = [
